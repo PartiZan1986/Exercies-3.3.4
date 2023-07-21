ee867c842953c05d5f8e93d633dbb3ce2e496470
Yaroslav <kay3@yandex.ru>
2023-07-21 16:11:11 +0300

    Add project files.

---
+++ Exercies 3.3.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Yaroslav";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
